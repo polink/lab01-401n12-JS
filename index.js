ebec663a166fe4662f8dfb8478c732d8797797cb
Karl Polintan <kf.polntan@gmail.com>
2019-02-08 23:30:27 -0800

    finished index

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const faker = require('faker');
 const greet = require('./lib/greet');
 const arithmetic = require('./lib/arithmetic');
 
+console.log(greet.sayHi(faker.name.firstName()));
+
+let a = faker.random.number();
+let b = faker.random.number();
+
+console.log(`Let's try some addition: ${a} plus ${b} is ${arithmetic.add(a,b)}!`);
+console.log(`Let's try some subtraction: ${a} minus ${b} is ${arithmetic.subtraction(a,b)}.`);
+console.log(`Time for some multiplication: ${a} times ${b} is ${arithmetic.multiply(a,b)}.`);
+console.log(`Now for some division: ${a} divided by ${b} is ${arithmetic.divide(a,b)}`);
